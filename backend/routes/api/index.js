dba59baea37bfffa9161bbabbae82de8434b8390
surnan <suresh_nandlall@outlook.com>
2024-05-29 19:43:26 -0400

    Completed both routes for deleting spot & review Images

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,8 @@ const sessionRouter   = require('./session.js');
 const usersRouter     = require('./users.js');
 const spotsRouter     = require('./spots.js');
 const reviewsRouter     = require('./reviews.js');
+const spotImagesRouter     = require('./spotImages.js');
+const reviewImagesRouter     = require('./reviewImages.js');
 
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -13,6 +15,8 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/spot-images', spotImagesRouter);
+router.use('/review-images', reviewImagesRouter);
 
 
 
---
+++ backend/routes/api/spotImages.js
@@ -0,0 +1,30 @@
+// backend/routes/api/spot-images.js
+const express = require('express');
+const { Spot, Review, Booking, SpotImage, ReviewImage, User } = require('../../db/models');
+const { Model } = require('sequelize');
+const { requireAuth } = require('../../utils/auth');
+
+const router = express.Router();
+
+// router.delete('/:imageId', requireAuth, async(req, res, next) =>{
+router.delete('/:imageId', async(req, res, next) =>{
+    try {
+        const {imageId} = req.params;
+        const spotImage = await SpotImage.findByPk(imageId);
+        if(!spotImage){
+           res.status(404).json({
+            message: "Spot image couldn't be found"
+          });
+         }
+        
+        await spotImage.destroy();
+         return res.json({ message: 'Deleted successfully!'});
+    } catch (e) {
+        next(e)
+    }
+ });
+
+
+
+
+module.exports = router;
