0fb22b5e550562839f58057b614d8c0646c24da9
surnan <suresh_nandlall@outlook.com>
2024-05-29 22:34:14 -0400

    upgraded Reviews-migration file

--- backend/db/migrations/03-Reviews.js
+++ backend/db/migrations/03-Reviews.js
@@ -28,6 +28,7 @@ module.exports = {
         references: {model: 'Spots'},
         onDelete: 'CASCADE'
       },
+      
       review: {
         type: Sequelize.STRING,
         allowNull: true
@@ -44,7 +45,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Reviews";
