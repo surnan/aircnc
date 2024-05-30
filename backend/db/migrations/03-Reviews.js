9638bcda7fdd66e3fbe989e7054de52305fd6e0b
surnan <suresh_nandlall@outlook.com>
2024-05-29 21:44:18 -0400

    Adjusted 03-Reviews migration file to overcome Render build errors

--- backend/db/migrations/03-Reviews.js
+++ backend/db/migrations/03-Reviews.js
@@ -47,6 +47,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Reviews');
+    options.tableName = "Reviews";
+    return queryInterface.dropTable(options);
   }
 };
