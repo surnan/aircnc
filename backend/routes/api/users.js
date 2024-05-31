9feb5c029e6168a9b2b1e43420654dbab45aa357
surnan <suresh_nandlall@outlook.com>
2024-05-31 00:29:41 -0400

    fixed api/users = Sign Up a User

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -33,19 +33,30 @@ const validateSignup = [
         .exists({ checkFalsy: true })
         .isLength({ min: 6 })
         .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 1 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 1 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+
     handleValidationErrors
 ];
 
 // Sign up
 router.post('/', validateSignup, async (req, res) => {
-    const { email, password, username } = req.body;
+    const { email, password, username, firstName, lastName } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({ email, username, hashedPassword });
+    const user = await User.create({ email, username, hashedPassword, firstName, lastName });
 
     const safeUser = {
         id: user.id,
-        email: user.email,
-        username: user.username,
+        firstName,
+        lastName,
+        email,
+        username
     };
 
     await setTokenCookie(res, safeUser);
