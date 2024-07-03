caacc50d9004a4f56584a33c6e8192595d5a557b
surnan <suresh_nandlall@outlook.com>
2024-07-03 06:18:05 -0400

    postgreSQL now implemented in local development environment.  postgresSQL now the only db used in development and production

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,34 @@
 // backend/config/database.js
 const config = require('./index');
 
-module.exports = {
-  development: {
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+const schema = db.schema
+
+const development = [
+  {
     storage: config.dbFile,
     dialect: "sqlite",
     seederStorage: "sequelize",
     logQueryParameters: true,
     typeValidation: true
   },
+  {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+    seederStorage: 'sequelize'
+  }
+]
+
+
+module.exports = {
+  development: process.env.DB_DIALECT === 'postgres' ? development[1] : development[0],
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
@@ -24,3 +44,28 @@ module.exports = {
     }
   }
 };
+
+
+// module.exports = {
+//   development: {
+//     storage: config.dbFile,
+//     dialect: "sqlite",
+//     seederStorage: "sequelize",
+//     logQueryParameters: true,
+//     typeValidation: true
+//   },
+//   production: {
+//     use_env_variable: 'DATABASE_URL',
+//     dialect: 'postgres',
+//     seederStorage: 'sequelize',
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       }
+//     },
+//     define: {
+//       schema: process.env.SCHEMA
+//     }
+//   }
+// };
