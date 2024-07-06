3f3f0fc77e9b3a8070141d89d389beff1b727278
surnan <suresh_nandlall@outlook.com>
2024-07-06 18:49:52 -0400

    Created: ViewAllSpots component and Store File

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-
 import Navigation from './components/Navigation';
-
-
 import * as sessionActions from './store/session';
+import ViewAllSpots from "./components/ViewAllSpots";
+
 
 function Layout() {
   const dispatch = useDispatch();
