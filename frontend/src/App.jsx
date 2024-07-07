37df8a5c8461892fd22fcbc045178d5d133b0e74
surnan <suresh_nandlall@outlook.com>
2024-07-07 12:50:35 -0400

    Added SpotCard that correctly loads Spot data via thunk and displays on homepage "/" for ViewAllSpots. CSS needs work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
         path: "/",
         element:
           <>
-            <ViewAllSpots />,
+            <ViewAllSpots />
           </>
       },
       {
---
+++ frontend/src/components/SpotCard/SpotCard.jsx
@@ -0,0 +1,32 @@
+//frontend/src/components/SpotCard/SpotCard.jsx
+import "./SpotCard.css";
+import { useNavigate } from "react-router-dom";
+
+
+function SpotCard({ spot }) {
+    const nav = useNavigate();
+    const { id, city, state, avgRating, price, name, previewImage } = spot
+
+    const starPath = "assets/icons/star.png"
+
+    return (
+        <div className="container">
+          <div className="spotCard">
+            <img key={id} className="clickable spotPreviewImage" src={previewImage} alt="House" />
+            <span className="tooltip">{name}</span>
+      
+            <div className="spotCardBtm">
+              <div className="spotCardBtm left">
+                <p>{city}, {state}</p>
+              </div>
+              <div className="spotCardBtm right">
+                <img key={id} className="starIcon" src={starPath} alt="star" />
+                {avgRating}
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+}
+
+export default SpotCard;
